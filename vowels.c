0baba1918525229dcdd51774cca2a961f89e6277
priya928 <52189783+priya928@users.noreply.github.com>
2019-07-12 20:48:57 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char ch;
+scanf("%c",&ch);
+if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+{
+printf("vowel");
+}
+else
+{
+printf("Invalid");
+}
+return 0;
+}
