3e492e45ae2152e2396c4e5c85ee1bbe952e7ebf
priya928 <52189783+priya928@users.noreply.github.com>
2019-07-14 20:14:43 +0530

    Rename oi.c to oi1.c

---
+++ oi1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int p,n;
+	scanf("%d %d",&p,&n);
+	for(int i=0;i<=n;i++)
+	{
+		if(i%2!=0)
+		{
+			printf("%d",i);
+		}
+	}
+	return 0;
+}
